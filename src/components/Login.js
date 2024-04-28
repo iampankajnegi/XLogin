782a4384d107bbafb51559bf33344c6053380890
unknown <negi60239@gmail.com>
2024-04-28 23:43:26 +0530

    task1

---
+++ src/components/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+   const [username , setUser ] = useState("")
+
+   const[password , setPass] = useState("")
+
+   const [message , setMessage] = useState("")
+
+   const handleUserChange = (e) =>{
+
+       let {name , value } = e.target
+
+       setUser({...preState ,[name]:value})
+   }
+
+   const handleStateChange = (e) =>{
+
+    let {name , value } = e.target
+
+    setPass({...preState ,[name]:value})
+   }
+
+  const handleSubmit =(e)=>{
+e.preventDefault();
+
+if(username === "User" && password === "Password"){
+
+    setMessage("Welcome, user!");
+}
+  else{
+
+     setMessage("Invalid username or password");
+  }     
+  }
+
+  return (
+    <div>
+        <div className="login">
+            <h1>Login Page</h1>
+        </div>
+        <div className="Login-form">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="Username">Username</label>
+                    <input type="text" value={username} name ="Username"  onChange={handleUserChange} required/>
+
+                    <label htmlFor="Password">Password</label>
+                    <input type="password" value={password} name="Password"  onChange={handleStateChange} required />
+                </div>
+
+                <button type="submit">Submit</button>
+                
+            </form>
+     
+              <div className='result'>{message}</div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
